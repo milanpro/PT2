3bf2aac1e0c6ee3c33aad00fe2e66125eb46a88f
Milan Proell <milan.proell@student.hpi.de>
2016-06-27 15:03:41 +0200

    5.Z: CustomDataStructure implemented

--- assignment5_sources/src/assignment5/Archeology.java
+++ assignment5_sources/src/assignment5/Archeology.java
@@ -10,8 +10,22 @@ public class Archeology {
 				+ "shaman rituals. Danger looms. Do you dare to go forward?");
 	}
 	public static ArrayList<WordPair> findCombinations(Shard[] shards){
-		//TODO: find all the shard pairs
-		
+        ArrayList<CustomDataStructure> normalshards = new ArrayList<>();
+        ArrayList<CustomDataStructure> reversedshards = new ArrayList<>();
+        Shard rev;
+        CustomDataStructure nShard = new CustomDataStructure();
+        CustomDataStructure rShard = new CustomDataStructure();
+        for(Shard Sha: shards){
+            rev = Sha.reverseShard();
+            for(int i=0;i < Sha.length();i++){
+                nShard.SetValue(Sha.getFirstChar());
+                rShard.SetValue(rev.getFirstChar());
+            }
+            normalshards.add(nShard);
+            reversedshards.add(rShard);
+        }
+
+
 		return new ArrayList<WordPair>();
 	}
 }
--- assignment5_sources/src/assignment5/CustomDataStructure.java
+++ assignment5_sources/src/assignment5/CustomDataStructure.java
@@ -2,5 +2,31 @@ package assignment5;
 
 
 public class CustomDataStructure {
-	//For your use
+    private Character value = null;
+    private CustomDataStructure Node = null;
+
+
+    //Getter - Setter functions
+    public void SetValue(char val){
+        if(Node!= null){
+            Node.SetValue(val);
+        }
+        else if(value!=null){
+            Node = new CustomDataStructure();
+            Node.SetValue(val);
+        }
+        else value = val;
+    }
+
+    public char GetValue(int index){
+        if(index == 0) return value;
+        else if(Node==null) throw new IndexOutOfBoundsException();
+        else return Node.GetValue(index-1);
+    }
+    //Empty the Structure
+    public void eraseAll(){
+        value = null;
+        Node = null;
+    }
+    
 }
