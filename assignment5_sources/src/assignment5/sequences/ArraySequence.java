381bc46df7913f832dad1acaf9afc3ffeabf075e
Milan Proell <milan.proell@student.hpi.de>
2016-06-28 14:58:30 +0200

    ArraySequence Implemented -> just IndexOutOfBoundException missing (HiddenTest)

--- assignment5_sources/src/assignment5/sequences/ArraySequence.java
+++ assignment5_sources/src/assignment5/sequences/ArraySequence.java
@@ -17,46 +17,73 @@ public class ArraySequence<T> implements Sequence<T>, List<T>, Stack<T>, Queue<T
 
     @Override
     public void insert(int index, T element) {
-
+        if(index>values.length||index<0){
+            throw new IndexOutOfBoundsException();
+        }
+        T[] tempvalues = values;
+        this.values = (T[]) new Object[tempvalues.length+1];
+        for (int i = 0;i<index;i++){
+            values[i] = tempvalues[i];
+        }
+        values[index] = element;
+        for (int i = index+1;i<values.length;i++){
+            values[i] = tempvalues[i-1];
+        }
     }
 
     @Override
     public T set(int index, T element) {
-        return null;
+        if(index>=values.length||index<0) throw new IndexOutOfBoundsException();
+        T temp = values[index];
+        values[index] = element;
+        return temp;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(index>=values.length||index<0) throw new IndexOutOfBoundsException();
+        return values[index];
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if(index>=values.length||index<0) throw new IndexOutOfBoundsException();
+        T[] tempvalues = values;
+        this.values = (T[]) new Object[tempvalues.length-1];
+        for(int i = 0;i<index;i++) values[i]=tempvalues[i];
+        for(int i = index+1;i<tempvalues.length;i++)values[i-1] = tempvalues[i];
+        return tempvalues[index];
     }
 
     @Override
     public void push_back(T element) {
-
-    }
+        T[] tempvalues = values;
+        this.values = (T[]) new Object[tempvalues.length+1];
+        for (int i = 0;i<tempvalues.length;i++){
+                this.values[i] = tempvalues[i];
+            }
+        this.values[tempvalues.length] = element;
+        }
 
     @Override
     public T pop_front() {
-        return null;
+        if(values.length==0) throw new NoSuchElementException();
+        return remove(0);
     }
 
     @Override
     public void push_top(T element) {
-
+        insert(0,element);
     }
 
     @Override
     public T pop_top() {
-        return null;
+        if(values.length==0) throw new NoSuchElementException();
+        return remove(0);
     }
 
     @Override
     public int size() {
-        return 0;
+        return values.length;
     }
 }
