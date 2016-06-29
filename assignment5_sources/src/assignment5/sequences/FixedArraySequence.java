c4f1b4ae906fe9d0fdf9ebc100d708f9d88b0678
Milan Proell <milan.proell@student.hpi.de>
2016-06-29 10:52:42 +0200

    FixedArraySequence implemented

--- assignment5_sources/src/assignment5/sequences/FixedArraySequence.java
+++ assignment5_sources/src/assignment5/sequences/FixedArraySequence.java
@@ -10,26 +10,28 @@ import java.util.NoSuchElementException;
  * @see <a href="https://en.wikipedia.org/wiki/Circular_buffer">https://en.wikipedia.org/wiki/Circular_buffer</a>
  */
 public class FixedArraySequence<T> extends ArraySequence<T> implements FixedSequence<T>, Deque<T> {
+    private int maximumsize;
     /**
      * @param maxSize The maximum number of elements this sequence can hold.
      * @throws IllegalArgumentException if maxSize is negative.
      */
     public FixedArraySequence(int maxSize) {
-
+        maximumsize = maxSize;
     }
 
     @Override
     public void push_front(T element) {
-
+        if(this.size()<maximumsize) super.push_top(element);
+        else throw new IllegalStateException();
     }
 
     @Override
     public T pop_back() {
-        return null;
+        return super.remove(this.size()-1);
     }
 
     @Override
     public int maximumSize() {
-        return 0;
+        return maximumsize;
     }
 }
